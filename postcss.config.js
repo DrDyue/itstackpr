6898a454b995d008e142d426ef9cad002dda4351
Viesis <silkansdominiks@gmail.com>
2026-03-19 11:43:26 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -4,13 +4,3 @@ export default {
         autoprefixer: {},
     },
 };
-
-
-
-
-
-
-skaties tagad aprsktu problemu ar auditu, nonem sitos 4 lielos konteinerus ar informaciju par kopa filtreti, sodien un kritiski, vinus parvieto tur pie konteirea ar lielo uzrakstu audita zurnals tur tadu mazinu ka uzraksta administratora zurnals vari apskatities ka tam ir jaizskatas ierices tabula kur ir rakstits cik kopa ir iericu, cik aktivas, renotna undefined.t.t un nonem informaciju par filtreti nevajag, tapat pastrada ar filtiem saja tabula lai action butu realizets caur dropbox skaisto ka tabula ierices, un tas pats ar severity un entity un obligati partulko sos laukus latviešu valoda tapat pasa tabula tur kur ir aciton meigini skaisti konteinera ar krasam atdalit visus aciton un pievienot vinam savas atsveiskas ikonas un to pasu izdari ar entity un partulko visu latviesu valoda
-
-skaties vel viena liela problema kas attiecas uz visu projektu tas ir visi lauki kur ir datumi, pirkmkart tie lauki kur jaievada datums ir rakstit mm.dd.yyyy
-bet vajag latviskot to visu lai butu dienadiena.menesismenesis.gadsgadsgadsgads un vel svarigi taja lauka kas izlien kad izvelies datumu visu menesa nosaukumus ari vajag rakstīt latviešu valoda un pogas saja ui ari latviski lai butu, visatrak tev vajadzes izdarit pasam savu datuma izveles ui un nem vera ka tev vajag atrst visus datuma izveles laukus un redigeshanas un aizstats vinus ar pareizajam latviskotajam versijam
